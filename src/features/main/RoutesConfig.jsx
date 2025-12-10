95e1dc6afd2fc86fd1ab673fd3f837926adf4aea
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-10 13:27:53 +0200

    Add a / route that renders the RootLayout component and put the /signup route as its child route

--- src/features/main/RoutesConfig.jsx
+++ src/features/main/RoutesConfig.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Signup from '../auth/signup/Signup.jsx';
+import RootLayout from './RootLayout.jsx';
 
 const router = createBrowserRouter([
     {
-        path: '/signup',
-        element: <Signup />,
+        path: '/',
+        element: <RootLayout />,
+        children: [
+            {
+                path: 'signup',
+                element: <Signup />,
+            },
+        ],
     },
 ]);
 

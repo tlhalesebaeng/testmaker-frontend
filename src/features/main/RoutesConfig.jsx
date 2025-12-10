17f0e04df069ce4e53d745582e75a52c8b862041
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-10 13:31:32 +0200

    Use the ErrorPage component as the error element of the / route

--- src/features/main/RoutesConfig.jsx
+++ src/features/main/RoutesConfig.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Signup from '../auth/signup/Signup.jsx';
 import RootLayout from './RootLayout.jsx';
+import ErrorPage from '../errors/ErrorPage.jsx';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <RootLayout />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: 'signup',

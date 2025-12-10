fc9229b1d797f6063c41d842b6f539cfceff9f05
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-10 13:23:32 +0200

    Import the router configurations and provide them to the application using the RouterProvider component

--- src/features/main/App.jsx
+++ src/features/main/App.jsx
@@ -1,7 +1,9 @@
+import { RouterProvider } from 'react-router-dom';
+import router from './RoutesConfig.jsx';
 import './App.css';
 
 const App = () => {
-    return <h1>Test Maker</h1>;
+    return <RouterProvider router={router} />;
 };
 
 export default App;

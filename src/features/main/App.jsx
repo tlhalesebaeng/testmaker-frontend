1832a05c24517c77a30d202723d94d34a34b61b2
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-11 14:18:11 +0200

    Delete the app styles and remove the imports

--- src/features/main/App.jsx
+++ src/features/main/App.jsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './RoutesConfig.jsx';
-import './App.css';
 
 const App = () => {
     return <RouterProvider router={router} />;

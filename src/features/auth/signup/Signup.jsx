3b295a340e71d70c7c63298d58044d60d9693809
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-11 22:02:49 +0200

    Add a create account button using the Button componet and add an onclick listener that prevents the default behaviour of the form element

--- src/features/auth/signup/Signup.jsx
+++ src/features/auth/signup/Signup.jsx
@@ -1,8 +1,13 @@
+import Button from '../../../components/button/Button.jsx';
 import Card from '../../../components/card/Card.jsx';
 import Input from '../../../components/input/Input.jsx';
 import './Signup.css';
 
 const Signup = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
     const fields = [
         { id: 'signup-field-1', type: 'text', placeholder: 'Enter your username', labelText: 'Username' },
         { id: 'signup-field-2', type: 'email', placeholder: 'example@testmaker.com', labelText: 'Email' },
@@ -20,6 +25,7 @@ const Signup = () => {
                     {fields.map((field) => (
                         <Input key={field.id} {...field} />
                     ))}
+                    <Button onClick={(e) => handleSubmit(e)}>Create Account</Button>
                 </form>
             </Card>
         </div>

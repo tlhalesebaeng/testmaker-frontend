3162f5388f934456cba8aea62ae4deb2517e48c5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-11 17:09:22 +0200

    Use the custom flex class names to center the content of the signup card along the main axis

--- src/features/auth/signup/Signup.jsx
+++ src/features/auth/signup/Signup.jsx
@@ -4,7 +4,7 @@ import './Signup.css';
 const Signup = () => {
     return (
         <div className="signup flex-container center">
-            <Card className="signup__card">
+            <Card className="signup__card flex-container justify-center">
                 <h2>Signup</h2>
             </Card>
         </div>

db78a4b732f24837160d247f775f42278267a817
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-11 18:04:22 +0200

    Write a JSDoc comment that explains the props that the component destructures

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,10 @@
 import './Card.css';
 
+/**
+ * @param {JSX} children Elements that the component receives as children
+ * @param {string} className Used to add more class names to the div element
+ */
+
 const Card = ({ children, className }) => {
     return <div className={`card${className ? ' ' + className : ''}`}>{children}</div>;
 };

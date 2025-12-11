400d206baa5e976187681c9e87a879a22d34f582
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-11 18:05:11 +0200

    Destructure the labelText, type and placeholder props and use them relevantly

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,9 +1,17 @@
 import './Input.css';
 
-const Input = () => {
+/**
+ *
+ * @prop {string} type Attribute used on the input element to specify its type
+ * @prop {string} placeholder Input element attribute that specifies the placeholder shown on the element
+ * @prop {string} labelText The text that the label element display. This is also used to link the label to the input which implies that it should be unique
+ */
+
+const Input = ({ type, placeholder, labelText }) => {
     return (
         <div className="input-container">
-            <input />
+            <label htmlFor={labelText.toLowerCase()}>{labelText}</label>
+            <input id={labelText.toLowerCase()} type={type} placeholder={placeholder} />
         </div>
     );
 };

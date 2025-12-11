efe22832a1f04a0dd03e65a4ee7bd57f8848f86a
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-11 14:18:36 +0200

    Wrap the outlets with a main tag

--- src/features/main/RootLayout.jsx
+++ src/features/main/RootLayout.jsx
@@ -1,7 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
-    return <Outlet />;
+    return (
+        <main>
+            <Outlet />
+        </main>
+    );
 };
 
 export default RootLayout;

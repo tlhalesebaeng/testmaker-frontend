33bafa728801923f900d6f9e35c7f4eb85ec78dd
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-11 21:59:40 +0200

    Create an array of input fields that should be displayed on the signup page, map through it and create the input fields

--- src/features/auth/signup/Signup.jsx
+++ src/features/auth/signup/Signup.jsx
@@ -1,7 +1,14 @@
 import Card from '../../../components/card/Card.jsx';
+import Input from '../../../components/input/Input.jsx';
 import './Signup.css';
 
 const Signup = () => {
+    const fields = [
+        { id: 'signup-field-1', type: 'text', placeholder: 'Enter your username', labelText: 'Username' },
+        { id: 'signup-field-2', type: 'email', placeholder: 'example@testmaker.com', labelText: 'Email' },
+        { id: 'signup-field-3', type: 'password', placeholder: 'Enter your password', labelText: 'Password' },
+    ];
+
     return (
         <div className="signup flex-container center">
             <Card className="signup__card flex-container column">
@@ -9,6 +16,11 @@ const Signup = () => {
                 <p className="flex-container justify-center">
                     Please fill in your details below to create an account with us
                 </p>
+                <form>
+                    {fields.map((field) => (
+                        <Input key={field.id} {...field} />
+                    ))}
+                </form>
             </Card>
         </div>
     );

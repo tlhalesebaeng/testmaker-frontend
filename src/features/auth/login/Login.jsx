0f5c3c1960e2834fdb5dbd1a65d9c019337ca384
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-12 13:28:39 +0200

    Wrap the elements with a Card component that has relevant class names

--- src/features/auth/login/Login.jsx
+++ src/features/auth/login/Login.jsx
@@ -1,5 +1,11 @@
+import Card from '../../../components/card/Card.jsx';
+
 const Login = () => {
-    return <div className="max-width max-height flex-container center"></div>;
+    return (
+        <div className="max-width max-height flex-container center">
+            <Card className="auth__card flex-container column"></Card>
+        </div>
+    );
 };
 
 export default Login;

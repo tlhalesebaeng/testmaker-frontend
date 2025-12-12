c4e7a0b9ddfff4d0ac2641ed2c65ed7b58cdd897
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-12 10:27:34 +0200

    Use the max-width styles to make the width of elements 100%

--- src/features/main/RootLayout.jsx
+++ src/features/main/RootLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
     return (
-        <main>
+        <main className="max-width">
             <Outlet />
         </main>
     );

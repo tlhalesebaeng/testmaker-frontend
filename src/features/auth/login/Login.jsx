6f7930df929fbd036df498da890930f46d77449e
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-12 13:31:34 +0200

    Render relevant login fields inside a form component

--- src/features/auth/login/Login.jsx
+++ src/features/auth/login/Login.jsx
@@ -1,9 +1,34 @@
 import Card from '../../../components/card/Card.jsx';
+import Input from '../../../components/input/Input.jsx';
+import Form from '../form/Form.jsx';
 
 const Login = () => {
+    const fields = [
+        {
+            id: 'f-1',
+            type: 'text',
+            placeholder: 'Enter your username',
+            labelText: 'Username',
+        },
+        {
+            id: 'f-2',
+            type: 'password',
+            placeholder: 'Enter your password',
+            labelText: 'Password',
+        },
+    ];
+
+    const description = 'Please fill in your details below to access your account';
+
     return (
         <div className="max-width max-height flex-container center">
-            <Card className="auth__card flex-container column"></Card>
+            <Card className="auth__card flex-container column">
+                <Form heading="Login" description={description}>
+                    {fields.map((field) => (
+                        <Input key={field.id} {...field} />
+                    ))}
+                </Form>
+            </Card>
         </div>
     );
 };

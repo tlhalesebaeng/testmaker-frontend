634c03153cb2dc63dfb62a286f5d6244226ec8f7
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-12 10:17:55 +0200

    Destructure the children prop and render its result inside form tags

--- src/features/auth/form/Form.jsx
+++ src/features/auth/form/Form.jsx
@@ -1,15 +1,17 @@
 import './Form.css';
 
 /**
- * @prop {string} heading // The text displayed as a level 2 heading
- * @prop {string} description // A short description of what the user should do
+ * @prop {string} heading The text displayed as a level 2 heading
+ * @prop {string} description A short description of what the user should do
+ * @param {JSX} children Elements that the component receives as children and are rendered between form tags
  */
 
-const Form = ({ heading, description }) => {
+const Form = ({ heading, description, children }) => {
     return (
         <div className="form">
             <h2>{heading}</h2>
             <p>{description}</p>
+            <form>{children}</form>
         </div>
     );
 };

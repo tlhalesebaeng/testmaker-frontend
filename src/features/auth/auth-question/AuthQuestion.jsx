8cd670657af4d1c42350580bc401b878d55aaa9e
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-12 13:52:44 +0200

    Destructure the children, linkText and onClick props and use them relevantly on the component

--- src/features/auth/auth-question/AuthQuestion.jsx
+++ src/features/auth/auth-question/AuthQuestion.jsx
@@ -1,8 +1,15 @@
-const AuthQuestion = () => {
+/**
+ *
+ * @prop {JSX} children The elements that the components receive as children. This should be a text
+ * @prop {string} linkText What is displayed in the anchor tag
+ * @prop {Function} onClick A function that is triggered when the achor tag is clicked
+ */
+
+const AuthQuestion = ({ children, linkText, onClick }) => {
     return (
         <div>
-            <p></p>
-            <a></a>
+            <p>{children}</p>
+            <a onClick={onClick}>{linkText}</a>
         </div>
     );
 };

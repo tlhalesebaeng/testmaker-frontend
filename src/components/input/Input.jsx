0ac5b84c400ad6333e0f7837c0ac23997cd6139b
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-12 10:42:19 +0200

    Accept an onChange function as a prop and link it to the input field

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -5,13 +5,14 @@ import './Input.css';
  * @prop {string} type Attribute used on the input element to specify its type
  * @prop {string} placeholder Input element attribute that specifies the placeholder shown on the element
  * @prop {string} labelText The text that the label element display. This is also used to link the label to the input which implies that it should be unique
+ * @prop {Function} onChange A function that is executed when a change occurs on the input field
  */
 
-const Input = ({ type, placeholder, labelText }) => {
+const Input = ({ type, placeholder, labelText, onChange }) => {
     return (
         <div className="input-container">
             <label htmlFor={labelText.toLowerCase()}>{labelText}</label>
-            <input id={labelText.toLowerCase()} type={type} placeholder={placeholder} />
+            <input id={labelText.toLowerCase()} type={type} placeholder={placeholder} onChange={onChange} />
         </div>
     );
 };

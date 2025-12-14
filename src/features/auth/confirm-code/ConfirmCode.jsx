bc52a8d824af5ebe6ca456699af72b120bf52a1d
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 11:29:22 +0200

    Page views

--- src/features/auth/confirm-code/ConfirmCode.jsx
+++ src/features/auth/confirm-code/ConfirmCode.jsx
@@ -1,5 +1,27 @@
+import Button from '../../../components/button/Button.jsx';
+import Input from '../../../components/input/Input.jsx';
+import AuthContainer from '../auth-container/AuthContainer.jsx';
+import AuthQuestion from '../auth-question/AuthQuestion';
+import Form from '../form/Form.jsx';
+
 const ConfirmCode = () => {
-    return <h1>Confirm Code</h1>;
+    const fieldProps = {
+        type: 'text',
+        placeholder: 'Enter the code',
+        labelText: 'Code',
+    };
+
+    const description = 'Enter the one time 6-digit code sent to your email';
+
+    return (
+        <AuthContainer>
+            <Form heading="Confirm Code" description={description}>
+                <Input {...fieldProps} />
+                <Button>Confirm</Button>
+            </Form>
+            <AuthQuestion linkText="Resend">Didn't receive the code?</AuthQuestion>
+        </AuthContainer>
+    );
 };
 
 export default ConfirmCode;

92e1eea7c3575a101ba362b567e27dc25fc75540
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 10:41:57 +0200

    Create an onchange listener and handle the changes

--- src/features/auth/forgot-password/ForgotPassword.jsx
+++ src/features/auth/forgot-password/ForgotPassword.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Button from '../../../components/button/Button';
 import Card from '../../../components/card/Card.jsx';
 import Input from '../../../components/input/Input.jsx';
@@ -6,10 +8,21 @@ import Form from '../form/Form.jsx';
 import './ForgotPassword.css';
 
 const ForgotPassword = () => {
+    const [data, setData] = useState({ email: '' }); // Email input field data
+
+    const handleChange = (event, name) => {
+        setData((prevData) => {
+            const newData = { ...prevData };
+            newData[name] = event.target.value;
+            return newData;
+        });
+    };
+
     const fieldProps = {
         type: 'email',
         placeholder: 'Enter your email',
         labelText: 'Email',
+        onChange: (e) => handleChange(e, 'email'),
     };
 
     const description = 'Please enter your email address to reset your password';

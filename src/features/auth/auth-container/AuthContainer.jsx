792a52a63dde3124bd737e33cb3ec4a6a05f3a46
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 11:17:08 +0200

    Return a Card component wrapped inside a div

--- src/features/auth/auth-container/AuthContainer.jsx
+++ src/features/auth/auth-container/AuthContainer.jsx
@@ -1,5 +1,11 @@
+import Card from '../../../components/card/Card.jsx';
+
 const AuthContainer = () => {
-    return <h1>Auth Container</h1>;
+    return (
+        <div className="max-width max-height flex-container center">
+            <Card className="auth__card flex-container column"></Card>
+        </div>
+    );
 };
 
 export default AuthContainer;

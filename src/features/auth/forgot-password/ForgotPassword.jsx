c2047ecfd5c74a64f8ab73138111aa516e6ed488
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 10:39:55 +0200

    Add views of the page

--- src/features/auth/forgot-password/ForgotPassword.jsx
+++ src/features/auth/forgot-password/ForgotPassword.jsx
@@ -1,7 +1,30 @@
+import Button from '../../../components/button/Button';
+import Card from '../../../components/card/Card.jsx';
+import Input from '../../../components/input/Input.jsx';
+import AuthQuestion from '../auth-question/AuthQuestion';
+import Form from '../form/Form.jsx';
 import './ForgotPassword.css';
 
 const ForgotPassword = () => {
-    return <h1>ForgotPassword</h1>;
+    const fieldProps = {
+        type: 'email',
+        placeholder: 'Enter your email',
+        labelText: 'Email',
+    };
+
+    const description = 'Please enter your email address to reset your password';
+
+    return (
+        <div className="max-width max-height flex-container center">
+            <Card className="auth__card flex-container column">
+                <Form heading="Forgot Password?" description={description}>
+                    <Input {...fieldProps} />
+                    <Button>Send Code</Button>
+                </Form>
+                <AuthQuestion linkText="Login">Remember your password?</AuthQuestion>
+            </Card>
+        </div>
+    );
 };
 
 export default ForgotPassword;

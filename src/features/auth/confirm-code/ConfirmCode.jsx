f4a2b5b2d9aba1a3104c3e84df85626a86ad76f3
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 11:30:59 +0200

    Handle the input change on the code input field

--- src/features/auth/confirm-code/ConfirmCode.jsx
+++ src/features/auth/confirm-code/ConfirmCode.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Button from '../../../components/button/Button.jsx';
 import Input from '../../../components/input/Input.jsx';
 import AuthContainer from '../auth-container/AuthContainer.jsx';
@@ -5,10 +7,21 @@ import AuthQuestion from '../auth-question/AuthQuestion';
 import Form from '../form/Form.jsx';
 
 const ConfirmCode = () => {
+    const [data, setData] = useState({ code: '' }); // Code input field data
+
+    const handleChange = (event, name) => {
+        setData((prevData) => {
+            const newData = { ...prevData };
+            newData[name] = event.target.value;
+            return newData;
+        });
+    };
+
     const fieldProps = {
         type: 'text',
         placeholder: 'Enter the code',
         labelText: 'Code',
+        onChange: (e) => handleChange(e, 'code'),
     };
 
     const description = 'Enter the one time 6-digit code sent to your email';

b8b1921cdd30e16b8cc19da885f78cf63ffaad31
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 22:16:01 +0200

    Add an image element and display it on top of the input element

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,18 +1,22 @@
 import './Input.css';
 
 /**
- *
  * @prop {string} type Attribute used on the input element to specify its type
  * @prop {string} placeholder Input element attribute that specifies the placeholder shown on the element
  * @prop {string} labelText The text that the label element display. This is also used to link the label to the input which implies that it should be unique
+ * @prop {string} imgSrc The source of the image
  * @prop {Function} onChange A function that is executed when a change occurs on the input field
+ * @prop {Function} onImageClick A function that defines what happens when the image is clicked
+ *
  */
 
-const Input = ({ type, placeholder, labelText, onChange }) => {
+const Input = ({ type, placeholder, labelText, onChange, imgSrc, onImageClick }) => {
+    const label = labelText ? labelText.toLowerCase() : undefined;
     return (
         <div className="input-container">
-            <label htmlFor={labelText.toLowerCase()}>{labelText}</label>
-            <input id={labelText.toLowerCase()} type={type} placeholder={placeholder} onChange={onChange} />
+            <label htmlFor={label}>{labelText}</label>
+            <input id={label} type={type} placeholder={placeholder} onChange={onChange} />
+            {imgSrc && <img onClick={onImageClick} src={imgSrc} alt={`${label}-img`} />}
         </div>
     );
 };

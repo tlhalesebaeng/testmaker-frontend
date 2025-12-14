611a046f69ec43a7c13736a6d172eedcbad187d6
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 11:18:44 +0200

    Accept the children prop and rendere its content inside the card component

--- src/features/auth/auth-container/AuthContainer.jsx
+++ src/features/auth/auth-container/AuthContainer.jsx
@@ -1,9 +1,14 @@
 import Card from '../../../components/card/Card.jsx';
 
-const AuthContainer = () => {
+/**
+ *
+ * @param {JSX} children Elements that the component receives as children and are rendered inside the Card component
+ */
+
+const AuthContainer = ({ children }) => {
     return (
         <div className="max-width max-height flex-container center">
-            <Card className="auth__card flex-container column"></Card>
+            <Card className="auth__card flex-container column">{children}</Card>
         </div>
     );
 };

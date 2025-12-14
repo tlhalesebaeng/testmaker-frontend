6565cb9475e169eedf14e2ee2e109afd03affcb3
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 13:35:40 +0200

    Page views

--- src/features/auth/change-password/ChangePassword.jsx
+++ src/features/auth/change-password/ChangePassword.jsx
@@ -1,7 +1,34 @@
+import Button from '../../../components/button/Button.jsx';
+import Input from '../../../components/input/Input.jsx';
+import AuthContainer from '../auth-container/AuthContainer.jsx';
+import Form from '../form/Form.jsx';
 import './ChangePassword.css';
 
 const ChangePassword = () => {
-    return <h1>Change Password</h1>;
+    const fields = [
+        {
+            id: 'f-1',
+            type: 'password',
+            placeholder: 'Enter your new password',
+            labelText: 'Password',
+        },
+        {
+            id: 'f-2',
+            type: 'password',
+            placeholder: 'Confirm your new password',
+            labelText: 'Confirm Password',
+        },
+    ];
+    return (
+        <AuthContainer>
+            <Form heading="Create New Password" description="">
+                {fields.map((field) => (
+                    <Input key={field.id} {...field} />
+                ))}
+                <Button>Change Password</Button>
+            </Form>
+        </AuthContainer>
+    );
 };
 
 export default ChangePassword;

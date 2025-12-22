a1e3a315903e0ab187c71d1e872286a4fcc7d5c5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-22 15:30:54 +0200

    Add a loading prop, disable the button and render a loading text when this prop is true

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -4,13 +4,14 @@ import './Button.css';
  * @prop {JSX} children Elements that the component receives as children. They are rendered between the button tags
  * @prop {Function} onClick A function triggered when the button is clicked
  * @prop {boolean} disabled Determines whether the button is clickable or not
+ * @prop {boolean} loading A loading state is shown when this boolean is true otherwise children are rendered
  */
 
-const Button = ({ children, onClick, disabled }) => {
+const Button = ({ children, onClick, disabled, loading }) => {
     return (
         <div className="button-container">
-            <button onClick={onClick} disabled={disabled}>
-                {children}
+            <button onClick={onClick} disabled={loading || disabled}>
+                {loading ? 'Loading...' : children}
             </button>
         </div>
     );

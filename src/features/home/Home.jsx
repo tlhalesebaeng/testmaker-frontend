97dd973eb78c59e43c589a5975d2de223e66faa1
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-24 19:20:11 +0200

    Add a logout button and send the logout request

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,7 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import { useFetch } from '../../hooks/useFetch.js';
+import Button from '../../components/button/Button.jsx';
 import './Home.css';
 
 const Home = () => {
-    return <h1>Home</h1>;
+    const navigate = useNavigate();
+    const { isLoading, fetch } = useFetch();
+
+    const handleLogout = async () => {
+        const response = await fetch('/auth/logout', 'get');
+        if (response) navigate('/');
+    };
+
+    return (
+        <div className="home">
+            <h1>Home</h1>
+            <Button loading={isLoading} onClick={handleLogout}>
+                Logout
+            </Button>
+        </div>
+    );
 };
 
 export default Home;

ff1031c3e1b75263f322619b6faa465783d508c5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-24 19:19:02 +0200

    Check whether the user is authenticated or not when we first load the app

--- src/features/main/RootLayout.jsx
+++ src/features/main/RootLayout.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useFetch } from '../../hooks/useFetch.js';
 
 const RootLayout = () => {
+    const [authChecked, setAuthChecked] = useState(false);
+    const { fetch } = useFetch();
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            const response = await fetch('/auth/check', 'get');
+            if (response) {
+                // TODO: Save the isAuth result on some global state management
+            }
+            setAuthChecked(true);
+        };
+        checkAuth();
+    }, []);
+
+    if (!authChecked) return <p>Loading...</p>;
+
     return (
         <main className="max-width">
             <Outlet />

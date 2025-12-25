7892337ea1bd47ad204b29a4e913aedc9d3ad690
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-25 20:50:29 +0200

    For all routes that are under /auth, navigate to the home page if the user is authenticated

--- src/features/auth/auth-container/AuthContainer.jsx
+++ src/features/auth/auth-container/AuthContainer.jsx
@@ -1,7 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
 import Card from '../../../components/card/Card.jsx';
 
 const AuthContainer = () => {
+    const isAuth = useSelector((state) => state.auth.isAuth);
+
+    if (isAuth) return <Navigate to="/home" />;
+
     return (
         <div className="max-width max-height flex-container center">
             <Card className="auth__card flex-container column">

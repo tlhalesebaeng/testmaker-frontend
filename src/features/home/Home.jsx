124822fc10d6e0ba46c758bf11c19055c7891c72
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-25 20:53:45 +0200

    Dispatch the logout actions when handling the logout button click

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,15 +1,21 @@
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { authActions } from '../../store/auth-slice.js';
 import { useFetch } from '../../hooks/useFetch.js';
 import Button from '../../components/button/Button.jsx';
 import './Home.css';
 
 const Home = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const { isLoading, fetch } = useFetch();
 
     const handleLogout = async () => {
         const response = await fetch('/auth/logout', 'get');
-        if (response) navigate('/');
+        if (response) {
+            dispatch(authActions.logout());
+            navigate('/');
+        }
     };
 
     return (

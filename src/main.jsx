51d8cce902255994e1bdd2aec478eac0c4f2f82b
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-25 20:06:38 +0200

    Provide the redux store to the application

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './features/main/App.jsx';
+import store from './store/index.js';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);

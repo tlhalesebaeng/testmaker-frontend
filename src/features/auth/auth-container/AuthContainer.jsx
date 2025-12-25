c0f4ff12dbb90e769524e6e6eff5f0c6f74a1ad8
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-25 20:43:25 +0200

    Remove the children prop and render the outlet component inside the card

--- src/features/auth/auth-container/AuthContainer.jsx
+++ src/features/auth/auth-container/AuthContainer.jsx
@@ -1,14 +1,12 @@
+import { Outlet } from 'react-router-dom';
 import Card from '../../../components/card/Card.jsx';
 
-/**
- *
- * @param {JSX} children Elements that the component receives as children and are rendered inside the Card component
- */
-
-const AuthContainer = ({ children }) => {
+const AuthContainer = () => {
     return (
         <div className="max-width max-height flex-container center">
-            <Card className="auth__card flex-container column">{children}</Card>
+            <Card className="auth__card flex-container column">
+                <Outlet />
+            </Card>
         </div>
     );
 };

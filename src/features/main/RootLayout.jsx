f67c1ab89117e3551907c1ba4f2ada9efe739381
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-25 20:22:35 +0200

    Dispatch the login action after successful auth check

--- src/features/main/RootLayout.jsx
+++ src/features/main/RootLayout.jsx
@@ -1,17 +1,18 @@
 import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import { useFetch } from '../../hooks/useFetch.js';
+import { authActions } from '../../store/auth-slice.js';
 
 const RootLayout = () => {
     const [authChecked, setAuthChecked] = useState(false);
     const { fetch } = useFetch();
+    const dispatch = useDispatch();
 
     useEffect(() => {
         const checkAuth = async () => {
             const response = await fetch('/auth/check', 'get');
-            if (response) {
-                // TODO: Save the isAuth result on some global state management
-            }
+            if (response && response.data) dispatch(authActions.login(response.data.user));
             setAuthChecked(true);
         };
         checkAuth();

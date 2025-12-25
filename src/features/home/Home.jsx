6b4982753d702e188ee6d91badfd743e3b72faf3
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-25 20:55:40 +0200

    Navigate to the login page when the user is not authenticated

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,5 +1,5 @@
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { authActions } from '../../store/auth-slice.js';
 import { useFetch } from '../../hooks/useFetch.js';
 import Button from '../../components/button/Button.jsx';
@@ -9,6 +9,7 @@ const Home = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const { isLoading, fetch } = useFetch();
+    const isAuth = useSelector((state) => state.auth.isAuth);
 
     const handleLogout = async () => {
         const response = await fetch('/auth/logout', 'get');
@@ -17,6 +18,8 @@ const Home = () => {
             navigate('/');
         }
     };
+
+    if (!isAuth) return <Navigate to="/auth/login" />;
 
     return (
         <div className="home">
